ce7fe0a5a6aa351978e667e567451fd0bbc1e7f1
JustusFT <JustusFT@gmail.com>
2017-04-27 16:02:03 +0800

    removed byebugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < Clearance::UsersController
   def create
     @user = User.new(user_from_params)
-    byebug
     if @user.save
-      byebug
       sign_in @user
       redirect_back_or url_after_create
     else
-      byebug
       render template: "users/new"
     end
   end
