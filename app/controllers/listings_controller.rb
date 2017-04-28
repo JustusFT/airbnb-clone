2beba65b92ef86efbeb229a201001f9099b78cf1
JustusFT <JustusFT@gmail.com>
2017-04-28 08:38:31 +0800

    added tagging feature for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -22,6 +22,6 @@ class ListingsController < ApplicationController
 
   private
   def strong_params
-    params.require(:listing).permit(:user_id, :description, :price, :address, :room_count, :bed_count, :guest_count)
+    params.require(:listing).permit(:user_id, :description, :price, :address, :room_count, :bed_count, :guest_count, :tag_list)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,4 @@
 class Listing < ApplicationRecord
   validates_presence_of :user_id, :price, :address, :room_count, :bed_count, :guest_count
+  acts_as_taggable
 end
