d55dfedd883cc8a7302d7246abef49589cc5ca19
JustusFT <JustusFT@gmail.com>
2017-05-01 17:33:50 +0800

    added host info on listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ user['password'] = 'asdf'
 user['password_confirmation'] = 'asdf'
 
 ActiveRecord::Base.transaction do
-  5.times do
+  2.times do
     user['full_name'] = Faker::Name.first_name
     user['email'] = Faker::Internet.email
     u = User.new(user)
-    # u.avatar = open(Faker::Avatar.image)
+    u.avatar = open(Faker::Avatar.image)
     u.save!
   end
 end
@@ -27,7 +27,7 @@ uids = []
 User.all.each { |u| uids << u.id }
 
 ActiveRecord::Base.transaction do
-  5.times do
+  2.times do
     listing['name'] = Faker::App.name
     listing['room_count'] = rand(0..5)
     listing['bed_count'] = rand(1..6)
@@ -43,7 +43,7 @@ ActiveRecord::Base.transaction do
     l = Listing.new(listing)
 
     random_photos = []
-    rand(1..4).times do
+    3.times do
       x = open(Faker::LoremPixel.image)
       random_photos << x
     end
