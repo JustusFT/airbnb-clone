da9a21ccd356daff5d2acc015c8e01e1b180d4b8
JustusFT <JustusFT@gmail.com>
2017-05-05 10:12:54 +0800

    refactored some more

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,12 +1,16 @@
 class BraintreeController < ApplicationController
+  before_action :make_booking
+
   def new
     @client_token = Braintree::ClientToken.generate
+    @listing = @booking.listing
+    @price = @listing.price * (@booking.check_out - @booking.check_in).to_f
   end
 
   def checkout
     nonce_from_the_client = params[:checkout_form][:payment_method_nonce]
 
-    price = Listing.find(session[:booking]["listing_id"]).price * (Date.parse(session[:booking]["check_out"]) - Date.parse(session[:booking]["check_in"])).to_f
+    price = @booking.listing.price * (@booking.check_out - @booking.check_in).to_f
 
     result = Braintree::Transaction.sale(
       :amount => price,
@@ -17,10 +21,20 @@ class BraintreeController < ApplicationController
     )
 
     if result.success?
-      Booking.new(session[:booking]).save!
+      @booking.save!
       redirect_to :root, :flash => { :success => "Transaction successful!" }
     else
       redirect_to :root, :flash => { :error => "Transaction failed. Please try again." }
+    end
+  end
+
+  private
+  def make_booking
+    @booking = Booking.new(session[:booking])
+
+    unless @booking.valid?
+      flash[:notice] = "ERROR, booking is not valid?"
+      redirect_to @booking.listing || "/listings"
     end
   end
 end
