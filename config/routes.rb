964b5b8568f9fcee43a9b65a82e8a69534fdc6ab
JustusFT <JustusFT@gmail.com>
2017-05-11 16:30:17 +0800

    restrict auto-generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,8 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pages
   resources :listings do
-    resources :bookings
+    resources :bookings, only: [:new, :create]
   end
   # resources :bookings
 
